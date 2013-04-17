717f79016d312f9b920e649e3392259b9c65e6d4
Lester Celestial <lesterc@sourcepad.com>
2013-04-17 21:43:34 +0800

    fix the && signs

--- recipes/source.rb
+++ recipes/source.rb
@@ -17,16 +17,16 @@ package dev_pkg
 remote_file imagemagick_url do
   source imagemagick_url
   path src_filepath
-  backup false
 end
 
 bash "compile_nginx_source" do
   cwd ::File.dirname(src_filepath)
   code <<-EOH
-    tar -xzvf #{::File.basename(src_filepath)} -C /tmp/imagemagick &&
-    cd /tmp/imagemagick &&
-    ./configure &&
-    make && make install &&
+    tar -xzvf #{::File.basename(src_filepath)} -C /tmp/imagemagick
+    cd /tmp/imagemagick
+    ./configure
+    make
+    make install
     ldconfig /usr/local/lib
   EOH
 end
