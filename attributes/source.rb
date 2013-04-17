e549d70e730ac26191823a3247474cef7b5c16d8
Lester Celestial <lesterc@sourcepad.com>
2013-04-18 04:33:10 +0800

    use http as source

--- attributes/source.rb
+++ attributes/source.rb
@@ -1,3 +1,3 @@
 default['imagemagick']['version'] = "6.8.4-10"
-default['imagemagick']['source']['url'] = "ftp://ftp.fifi.org/pub/ImageMagick/ImageMagick-#{default['imagemagick']['version']}.tar.gz"
+default['imagemagick']['source']['url'] = "http://www.imagemagick.org/download/ImageMagick-#{default['imagemagick']['version']}.tar.gz"
 default['imagemagick']['source']['checksum'] = "ad08175df0e76c5598d07e0cd80ebd9e9d0380ef4df0a13f2adbcd9e522619ef"
