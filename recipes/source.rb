49557d0a59374590e0b441d5bc56968527f8633f
Lester Celestial <lesterc@sourcepad.com>
2013-04-19 03:56:43 +0800

    fixed rescue handler

--- recipes/source.rb
+++ recipes/source.rb
@@ -18,12 +18,8 @@ remote_file imagemagick_url do
   source imagemagick_url
   checksum node['imagemagick']['source']['checksum']
   path src_filepath
-  backup false
-
-  not_if {File.exists?(src_filepath)}
+  not_if { File.exists?(src_filepath) }
 end
-
-node.run_state['imagemagick_force_recompile'] = false
 
 bash "compile_imagemagick_source" do
   cwd ::File.dirname(src_filepath)
@@ -38,7 +34,7 @@ bash "compile_imagemagick_source" do
 
   not_if do
     identify = Mixlib::ShellOut.new("identify -version")
-    identify.run_command rescue return true
+    identify.run_command rescue false
     identify.stdout.split("\n") == ["Version: ImageMagick 6.8.4-10 2013-04-18 Q16 http://www.imagemagick.org", "Copyright: Copyright (C) 1999-2013 ImageMagick Studio LLC", "Features: DPC OpenMP", "Delegates: bzlib djvu fontconfig freetype jng jp2 jpeg lcms lqr openexr pango png ps tiff x xml zlib"]
   end
 
