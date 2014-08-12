ced1527f404522f53e62c769e7766da01eca7c95
alainchiasson <alain@chiasson.org>
2014-08-12 11:01:57 -0400

    Update metadata.rb
    Added explicit default tag for recipe. So that it can be loaded into rightscale.

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ description      "Installs/Configures imagemagick"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.2.2"
 
-recipe "imagemagick", "Installs imagemagick package"
+recipe "imagemagick::default", "Installs imagemagick package"
 recipe "imagemagick::devel", "Installs imagemagick development libraries"
 recipe "imagemagick::rmagick", "Installs rmagick gem"
 
